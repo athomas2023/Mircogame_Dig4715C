4a1510df7a84bf540054a24ea1c03ec022f595ae
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2023-01-22 21:25:14 -0500

    final

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -11,6 +11,7 @@ public class GameHandler : MonoBehaviour
     public float charge_TimeValue = 12;
     public TextMeshProUGUI Charge_valuetext;
     int chargevalue = 0;
+    public TextMeshProUGUI Needvalue;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -20,16 +21,30 @@ public class GameHandler : MonoBehaviour
     public GameObject startText;
     public GameObject lostText;
     public GameObject Esctext;
+    public GameObject winText;
 
+    public GameObject Octo;
+    public GameObject power;
+
+    public int needed = 50;
+
+    int tries = 0;
+
+    public AudioClip winmusic;
+    public AudioClip lostmusic;
+    public AudioClip starSound;
     
+    AudioSource audioSource;
 
 
     // Start is called before the first frame update
     void Start()
     {
         charge_Text.text = charge_TimeValue.ToString();
-        Charge_valuetext = chargevalue.ToString();
+        Charge_valuetext.text = chargevalue.ToString();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        Needvalue.text =  needed.ToString();
+        audioSource = GetComponent<AudioSource>();
         
         StartCoroutine(Startgame());
 
@@ -44,12 +59,40 @@ public class GameHandler : MonoBehaviour
     {
         charge_TimeValue -= 1 * Time.deltaTime;
         charge_Text.text = charge_TimeValue.ToString("0");
-        Charge_valuetext = chargevalue.ToString();
+        Charge_valuetext.text = chargevalue.ToString();
+        Needvalue.text =  needed.ToString();
 
-        if ( charge_TimeValue <= 0.1)
+        if ( tries == 1)
         {
+            needed = 30;
+        }
+
+        if ( tries == 2)
+        {
+            needed = 60;
+        }
+
+        if ( tries == 3)
+        {
+            needed = 25;
+        }
+
+        if ( tries == 4)
+        {
+            needed = 25;
+        }
+        
+
+        if ( charge_TimeValue <= 0.1) 
+        {
+            if (chargevalue != needed)
+            {
+                PlaySound(lostmusic);
                 lostText.SetActive(true);
                 StartCoroutine(Restart());
+            }
+
+
         }
 
 
@@ -71,6 +114,17 @@ public class GameHandler : MonoBehaviour
             Application.Quit();
             Debug.Log("Game Closed");
         }
+
+        if ( chargevalue == needed)
+        {
+            PlaySound(winmusic);
+            power.SetActive(true);
+            Octo.SetActive(false);
+            winText.SetActive(true);
+            StartCoroutine(Restart());
+        }
+    
+        
     }
 
     void FixedUpdate()
@@ -84,9 +138,12 @@ public class GameHandler : MonoBehaviour
 
     IEnumerator Startgame()
     {
-        
+        PlaySound(starSound);
         yield return new WaitForSeconds(2);
+       
         startText.SetActive(false);
+        Octo.SetActive(true);
+        chargevalue = 0;
         
 
     }
@@ -95,9 +152,19 @@ public class GameHandler : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         lostText.SetActive(false);
+        winText.SetActive(false);
         startText.SetActive(true);
         Esctext.SetActive(true);
+        power.SetActive(false);
         charge_TimeValue = 10;
+        tries = tries + 1;
         StartCoroutine(Startgame());
+
+
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
