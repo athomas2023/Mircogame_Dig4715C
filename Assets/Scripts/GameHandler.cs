8667efc88ee95553ab8f69ff77d2b70a2383ee9e
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2023-01-22 20:06:15 -0500

    Second Commit

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -8,15 +8,32 @@ public class GameHandler : MonoBehaviour
 {
 
     public TextMeshProUGUI charge_Text;
-    public int charge_Value = 10;
+    public float charge_TimeValue = 12;
+    public TextMeshProUGUI Charge_valuetext;
+    int chargevalue = 0;
 
     Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    public float speed = 3.0f;
+
+    public GameObject startText;
+    public GameObject lostText;
+    public GameObject Esctext;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        charge_Text.text = charge_Value.ToString();
+        charge_Text.text = charge_TimeValue.ToString();
+        Charge_valuetext = chargevalue.ToString();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        
+        StartCoroutine(Startgame());
+
+
 
         // Random select Enviroment/ Change back ground
 
@@ -25,11 +42,25 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        charge_Text.text = charge_Value.ToString();
+        charge_TimeValue -= 1 * Time.deltaTime;
+        charge_Text.text = charge_TimeValue.ToString("0");
+        Charge_valuetext = chargevalue.ToString();
+
+        if ( charge_TimeValue <= 0.1)
+        {
+                lostText.SetActive(true);
+                StartCoroutine(Restart());
+        }
+
+
+
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
 
         if (Input.GetKeyDown(KeyCode.Space) )
         {
-            charge_Value = charge_Value + 1;
+            chargevalue = chargevalue + 1;
             Debug.Log("Charge was Pressed");
             
             
@@ -40,5 +71,33 @@ public class GameHandler : MonoBehaviour
             Application.Quit();
             Debug.Log("Game Closed");
         }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+
+    IEnumerator Startgame()
+    {
+        
+        yield return new WaitForSeconds(2);
+        startText.SetActive(false);
+        
+
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(2);
+        lostText.SetActive(false);
+        startText.SetActive(true);
+        Esctext.SetActive(true);
+        charge_TimeValue = 10;
+        StartCoroutine(Startgame());
     }
 }
